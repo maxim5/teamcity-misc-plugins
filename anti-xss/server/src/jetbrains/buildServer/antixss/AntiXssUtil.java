3816f2662f19477576f53b29bff7072518f52804
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2012-09-29 15:07:52 +0200

    tests

--- anti-xss/server/src/jetbrains/buildServer/antixss/AntiXssUtil.java
+++ anti-xss/server/src/jetbrains/buildServer/antixss/AntiXssUtil.java
@@ -4,12 +4,15 @@
  */
 package jetbrains.buildServer.antixss;
 
-import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
 
 /**
  * Inspired by this
  * <a href="http://ricardozuasti.com/2012/stronger-anti-cross-site-scripting-xss-filter-for-java-web-apps/">blog post</a>.
+ * See also
+ * <a href="https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet">XSS Filter Evasion Cheat Sheet</a>.
  *
  * @author Maxim Podkolzine (maxim.podkolzine@jetbrains.com)
  * @since 8.0
@@ -29,12 +32,23 @@ public class AntiXssUtil {
     // expression(...)
     // Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
     // javascript:...
-    // Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
+    Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
     // vbscript:...
     // Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
     // onload(...)=...
     Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
   };
+
+  // The pattern below catches these kinds of js injections:
+  //
+  //   <script>foo('${param}');</script>
+  //
+  // where ${param} is not properly escaped.
+  //
+  // But unfortunately the pattern is too strong to be used as is.
+  //
+  //  Pattern.compile("['\"].*\\).*", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
+  //
 
   public static boolean hasPotentialJsInjection(@NotNull String s) {
     // An assumption: a JS injection is possible only via HTML injection.
--- anti-xss/tests/src/jetbrains/buildServer/antixss/AntiXssUtilTest.java
+++ anti-xss/tests/src/jetbrains/buildServer/antixss/AntiXssUtilTest.java
@@ -13,14 +13,59 @@ import org.testng.annotations.Test;
  */
 @Test
 public class AntiXssUtilTest extends BaseTestCase {
-  public void test_find_js_injection() throws Exception {
+  public void test_find_script_injection() throws Exception {
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<script></script>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<SCRIPT></SCRIPT>"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<script>foo</script>"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <script>foo</script>"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<script>foo</script> foo"));
-
     assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <script type='text/javascript'>alert(1)</script> foo"));
 
+    // See TW-17745.
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("./../logs\"<script>alert(1)</script>/teamcity-server.log"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\"><script>alert(1)</script>"));
+  }
+
+  public void test_find_onload_injection() throws Exception {
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<img src='' onload=''>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar onload='' /> baz"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar ONLOAD='' /> baz"));
+  }
+
+  public void test_find_src_injection() throws Exception {
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<IMG SRC=`javascript:alert(\"RSnake says, 'XSS'\")`>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<IMG SRC=javascript:alert(\"XSS\")>"));
+  }
+
+  public void test_no_js_injection() throws Exception {
+    assertFalse(AntiXssUtil.hasPotentialJsInjection(""));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("bar"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("buildTypeId"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("projectId"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("tab"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("bt222"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("project222"));
+
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo bar onload='' baz"));
+  }
+
+  // Disabled until a proper pattern
+  private void test_find_js_injection() throws Exception {
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("')"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',0)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\")"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",0)"));
+
+    // See TW-17745.
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',);alert(1)//"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)});alert(1);//x%0aa({//"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("5f9b9');alert(1);//"));
+
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar' , 'baz'); alert(1); //"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar\" , 'baz'); alert(1); //"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\''); alert(1); //"));
   }
 }
