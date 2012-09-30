69d614dd7ecd90ce83544d22bd42b21be5968a67
maxim <smartmaxim@gmail.com>
2012-09-30 17:01:52 +0200

    xss util update + tests

--- anti-xss/tests/src/jetbrains/buildServer/antixss/AntiXssUtilTest.java
+++ anti-xss/tests/src/jetbrains/buildServer/antixss/AntiXssUtilTest.java
@@ -29,12 +29,49 @@ public class AntiXssUtilTest extends BaseTestCase {
   public void test_find_onload_injection() throws Exception {
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<img src='' onload=''>"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar onload='' /> baz"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar onload = '' /> baz"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar onload = \"\" /> baz"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar onload=alert(1) /> baz"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("foo <bar ONLOAD='' /> baz"));
   }
 
   public void test_find_src_injection() throws Exception {
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<IMG SRC=`javascript:alert(\"RSnake says, 'XSS'\")`>"));
     assertTrue(AntiXssUtil.hasPotentialJsInjection("<IMG SRC=javascript:alert(\"XSS\")>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<img src = javascript:alert(1)/>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<img src = 'javascript:alert(1)'/>"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("<img src = \"javascript:alert(1)\"/>"));
+  }
+
+  public void test_find_js_injection() throws Exception {
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("')"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',0)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar');"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(');"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(', 'foo');"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\\'(');"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\\'');"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(', '', '', '', '', '');"));
+
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\")"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",0)"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar\");"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\");"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(', \"foo\");"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("\\\"(\");"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\\\"\");"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\", \"\", \"\", \"\", \"\", \"\");"));
+
+    // See TW-17745.
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',);alert(1)//"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)});alert(1);//x%0aa({//"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("5f9b9');alert(1);//"));
+
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar' , 'baz'); alert(1); //"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar\" , 'baz'); alert(1); //"));
+    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\\''); alert(1); //"));
   }
 
   public void test_no_js_injection() throws Exception {
@@ -47,25 +84,22 @@ public class AntiXssUtilTest extends BaseTestCase {
     assertFalse(AntiXssUtil.hasPotentialJsInjection("bt222"));
     assertFalse(AntiXssUtil.hasPotentialJsInjection("project222"));
 
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("onload"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("ONLOAD"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo onload bar"));
     assertFalse(AntiXssUtil.hasPotentialJsInjection("foo bar onload='' baz"));
-  }
 
-  // Disabled until a proper pattern
-  private void test_find_js_injection() throws Exception {
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("')"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("',0)"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("\")"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",)"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("\",0)"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("img src"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("img src="));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("script"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("javascript"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("fooScriptBar"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("javascript:"));
 
-    // See TW-17745.
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("',);alert(1)//"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("',)});alert(1);//x%0aa({//"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("5f9b9');alert(1);//"));
-
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar' , 'baz'); alert(1); //"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("foo bar\" , 'baz'); alert(1); //"));
-    assertTrue(AntiXssUtil.hasPotentialJsInjection("(\''); alert(1); //"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("Don't be evil"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("(foo)"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo (bar) baz"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo ('bar') baz"));
+    assertFalse(AntiXssUtil.hasPotentialJsInjection("foo (\"bar\") baz"));
   }
 }
